9d8ad81d29bae1f40a18636d798fbffde477bfa4
山本翼 <bnp1125@gmail.com>
2015-05-14 10:01:02 +0900

    クリエイトタグ、受発注画面、新規・編集画面の作成

--- order/js/function.js
+++ order/js/function.js
@@ -5,7 +5,7 @@
  * 戻り値:なし
  * 概要  :クリックしたタブの背景色を白色にする
  * 作成日 :2015.05.07
- * 作成者:T.Masuda
+ * 作成者:T.Yamamoto
 */
 function tabColorChange() {
 	// タブをクリックした時の処理
@@ -36,7 +36,7 @@ function tabColorChange() {
  * 戻り値:なし
  * 概要  :ダイアログを作る
  * 作成日 :2015.05.12
- * 作成者:T.Masuda
+ * 作成者:T.Yamamoto
 */
 function createDialog(dialogName, openButton) {
 	// ダイアログの設定
@@ -65,10 +65,71 @@ function createDialog(dialogName, openButton) {
  * 戻り値:なし
  * 概要  :属性を削除する
  * 作成日 :2015.05.13
- * 作成者:T.Masuda
+ * 作成者:T.Yamamoto
 */
 function deleteAttr(selector, attr) {
 	// 第一引数の要素から第二引数の属性を削除する
 	$(selector).removeAttr(attr);
+}
+
+/* 
+ * 関数名:setOptionValue
+ * 引数  :なし
+ * 戻り値:なし
+ * 概要  :optionタグのvalue属性に値を入れる
+ * 作成日 :2015.05.13
+ * 作成者:T.Yamamoto
+*/
+function setOptionValue() {
+	// optionタグをループで全て操作する
+	$("select option").each(function(i){
+		// optionタグの文字列を変数に入れる
+		var selectValue = $(this).text();
+		// 取得した文字列をvalue属性に入れる
+		$(this).val(selectValue);
+	});
+}
+
+/* 
+ * 関数名:setAddButtonLink
+ * 引数  :
+ *   selector	: addbuttonがあるセレクター名
+ *   link  		: href属性の値
+ * 戻り値:なし
+ * 概要  :追加ボタンのhref属性をセットする
+ * 作成日 :2015.05.13
+ * 作成者:T.Yamamoto
+*/
+function setAddButtonLink(selector, link) {
+	// 第一引数のセレクターに対してaddButtonクラスを持つ親要素に第二引数の値をhref属性に設定する
+	$(selector).find('.addButton').parent().attr('href', link);
+}
+
+/* 
+ * 関数名:funcTab
+ * 引数 :なし
+ * 戻り値:なし
+ * 概要  :タブ機能を実装する
+ * 作成日 :2015.05.13
+ * 作成者:T.Yamamoto
+*/
+function funcTab() {
+	// デフォルトのコンテンツを表示するために。selectedクラスを持つタブのname属性を取得する
+	var defaultContent = $('.selected').attr('name');
+	// デフォルトのタブコンテンツを表示する
+	$('#' + defaultContent).css('display', 'block');
+	// タブがクリックされた時の処理
+	$('.tabOut').on('click', '.tablist a', function() {
+		// クリックされた要素のクラス名をを取得し、表示するコンテンツのid名として使う
+		var displayContent = $(this).attr('name');
+		// selectedクラスを全てのリストから取り除く
+		$('.tablist a').removeClass('selected');
+		// クリックした要素にselectedクラスを付け足す
+		$(this).addClass('selected');
+		// タブのコンテンツを全て非表示にする
+		$('.tabIn .tabcontent').css('display', 'none');
+		// クリックした要素に対応するコンテンツを表示する
+		$('#' + displayContent).css('display', 'block');
+	});
 }
 
